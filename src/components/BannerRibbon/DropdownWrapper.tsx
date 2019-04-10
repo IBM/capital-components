c92e878031de1b460b58f761b5b2a304d139f9ec
Paul Sachs <paul.sachs@ibm.com>
2019-04-10 15:01:10 -0400

    fix(BannerRibbon.DropdownWrapper): Typing adjustments
    Fix typing inferences. Will need to standardize on these.

--- src/components/BannerRibbon/DropdownWrapper.tsx
+++ src/components/BannerRibbon/DropdownWrapper.tsx
@@ -1,15 +1,22 @@
 import { cx } from "emotion";
-import React, { FC } from "react";
+import React from "react";
 import { mqStrings, mqStringsMax } from "../../layout/mediaQueries";
 import { styled } from "../../support/theme";
-import { Omit } from "type-zoo/types";
+
+const Internal = React.forwardRef<HTMLDivElement, JSX.IntrinsicElements["div"]>(
+  ({ className, ...props }, ref) => (
+    <div
+      className={cx(className, "cap-padding--horizontal", "cap-dropdown-wrapper")}
+      {...props}
+      ref={ref}
+    />
+  )
+);
 
 /* istanbul ignore next */
-/**
- * We have this wrapper because the carbon dropdown wants to shrink a lot.
- * This wrapper prevents that.
- */
-const DropdownWrapperInternal = styled.div`
+const DropdownWrapper = styled<
+  JSX.IntrinsicElements["div"] & { innerRef?: React.Ref<HTMLDivElement> }
+>(Internal)(`
   ${mqStringsMax.s(`
     max-width: 100%;
     padding-left: 0;
@@ -18,16 +25,10 @@ const DropdownWrapperInternal = styled.div`
   ${mqStrings.s(`
     min-width: 240px;
   `)}
-`;
+`);
 
-// /* istanbul ignore next */
-const DropdownWrapper: FC<
-  Omit<React.DetailedHTMLProps<React.HTMLAttributes<HTMLDivElement>, HTMLDivElement>, "ref">
-> = ({ className, ...props }) => (
-  <DropdownWrapperInternal
-    className={cx(className, "cap-padding--horizontal", "cap-dropdown-wrapper")}
-    {...props}
-  />
-);
-
+/**
+ * We have this wrapper because the carbon dropdown wants to shrink a lot.
+ * This wrapper prevents that.
+ */
 export default DropdownWrapper;
